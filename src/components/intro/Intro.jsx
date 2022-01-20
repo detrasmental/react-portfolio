afd43120da3171bf20ed60eaca1c6adbbb3f72fe
detrasmental <detra.gordon76@gmail.com>
2022-01-20 13:54:27 -0500

    trying to fix scrollbar

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -18,7 +18,7 @@ export default function Intro() {
         <div className="intro" id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="assets/detra.jpg" alt="me"/>
+                    <img src="assets/detranobg.png" alt="me"/>
                 </div>
             </div>
             <div className="right">
